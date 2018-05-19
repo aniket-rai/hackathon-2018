d9e192a343506407ac7f329e789f97ee546e341f
Aniket Rai <aniketrai@icloud.com>
2018-05-19 16:41:48 +1200

    progress! part 2

--- action.php
+++ action.php
@@ -1,9 +1,16 @@
 <?php
-  $txt= "data.txt";
-  if (isset($_POST['c1']) && isset($_POST['c2']) && isset($_POST['c3']) && isset($_POST['c4']) && isset($_POST['c5'])) {
-  $fh = fopen($txt, 'a');
-   $txt=$_POST['c1'].' - '.$_POST['c2'].' - '.$_POST['c3'].' - '.$_POST['c4'].' - '.$_POST['c5'];
-   fwrite($fh,$txt); // Write information to the file
-   fclose($fh); // Close the file
-  }
- ?>
+$file = 'data.txt';
+$email = $_POST['c1'];
+$fp = fopen("data.txt", "a");
+$savestring = $email . "\n";
+fwrite($fp, $savestring);
+fclose($fp);
+echo "<h1>Thank you, we will be in touch as soon as possible!</h1>";
+
+$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+$txt = "John Doe\n";
+fwrite($myfile, $txt);
+$txt = "Jane Doe\n";
+fwrite($myfile, $txt);
+fclose($myfile);
+?>
